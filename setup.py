54dfb80661074cc0f072435a4ba6cddd83385192
StFreve <Golfik2007@gmail.com>
2024-09-30 16:51:10 +0400

    Update setup.py to have the same list of parameters as main.py

--- setup.py
+++ setup.py
@@ -13,8 +13,6 @@ def create_service_file(args):
         command += f"--pc-address {args.pc_address} "
     if args.interval:
         command += f"--interval {args.interval} "
-    if args.use_play_state:
-        command += f"--use-play-state "
     if args.additional_args:
         command += f"{' '.join(args.additional_args)}"
     service_content = f"""[Unit]
@@ -50,13 +48,12 @@ def setup_service(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Setup JBL Keeper Service")
-    parser.add_argument('--pc-address', default=None, help="IP address of the PC (optional). If you are running the script on the same machine where the JBL speaker is connected, you can ignore this option.")
+    parser.add_argument('--pc-address', help="IP address of the PC (optional). If you are running the script on the same machine where the JBL speaker is connected, you can ignore this option.")
     parser.add_argument('--jbl-address', required=True, help="IP address of the JBL speaker")
-    parser.add_argument('--jbl-port', default=80, type=int, help="Port of the JBL speaker (default: 80)")
-    parser.add_argument('--jbl-pin', default=1234, type=int, help="PIN of the JBL speaker (default: 1234)")
+    parser.add_argument('--jbl-port', type=int, help="Port of the JBL speaker (default: 80)")
+    parser.add_argument('--jbl-pin', type=int, help="PIN of the JBL speaker (default: 1234)")
     parser.add_argument('--user', default=os.getlogin(), help="User to run the service (default: current user)")
-    parser.add_argument('--interval', default=60, type=int, help="Interval to send keep alive requests (default: 60)")
-    parser.add_argument('--use-play-state', action='store_true', help="Use play state to turn on/off the JBL speaker")
+    parser.add_argument('--interval', type=int, help="Interval to send keep alive requests (default: 60)")
     parser.add_argument('additional_args', nargs=argparse.REMAINDER, help="Additional arguments for main.py")
     
     args = parser.parse_args()
