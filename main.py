e33aea1181d75f7d91ec8e0d13556c383e55dd7a
StFreve <Golfik2007@gmail.com>
2024-11-22 14:34:51 +0400

    Automatically reset session if previous one was removed for some reason

--- main.py
+++ main.py
@@ -48,9 +48,7 @@ class JBLSpeaker:
         self.session_id = None
 
     def __del__(self):
-        if self.session_id:
-            self._delete_session()
-            self.session_id = None
+        self._delete_session()
 
     def _get_session_id(self):
         response = requests.get(f"{self.base_url}/CREATE_SESSION?pin={self.pin}")
@@ -58,67 +56,71 @@ class JBLSpeaker:
             self.session_id = response.text.split("<sessionId>")[1].split("</sessionId>")[0]
     
     def _delete_session(self):
-        requests.get(f"{self.base_url}/DELETE_SESSION?pin={self.pin}&sid={self.session_id}")
+        if self.session_id:
+            requests.get(f"{self.base_url}/DELETE_SESSION?pin={self.pin}&sid={self.session_id}")
+            self.session_id = None
     
-    def get_power_state(self):
+    def _reset_session(self):
+        self._delete_session()
+        self._get_session_id()
+    
+    def request(self, operation, node, params = None, reset_session=True):
         if not self.session_id:
             self._get_session_id()
-        response = requests.get(f"{self.base_url}/GET/netRemote.sys.power?pin={self.pin}&sid={self.session_id}")
+
+        params_str = "&".join([f"{k}={v}" for k, v in params.items()]) if params else ""
+        response = requests.get(f"{self.base_url}/{operation}/{node}?pin={self.pin}&sid={self.session_id}&{params_str}")
+        if response.status_code == 200 :
+            return response
+
+        try:
+            self._reset_session()
+            return self.request(operation, node, params, reset_session=False)
+        except Exception as e:
+            logger.exception(f"Failed to reset session: {e}")
+        raise Exception(f"Failed to send request to {operation}/{node}")
+
+
+    def get_power_state(self):
+        response = self.request("GET", "netRemote.sys.power")
         if response.status_code == 200:
             return int(response.text.split("<value>")[1].split("</value>")[0])
         return None
 
     def set_power_state(self, state):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/SET/netRemote.sys.power?pin={self.pin}&sid={self.session_id}&value={state}")
-        return response.status_code == 200
+        return self.request("SET", "netRemote.sys.power", {"value": state}).status_code == 200
 
     def get_play_status(self):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/GET/netRemote.play.status?pin={self.pin}&sid={self.session_id}")
+        response = self.request("GET", "netRemote.play.status")
         if response.status_code == 200:
             return int(response.text.split("<u8>")[1].split("</u8>")[0])
         return None
     
     def get_play_state(self):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/GET/netRemote.play.control?pin={self.pin}&sid={self.session_id}")
+        response = self.request("GET", "netRemote.play.control")
         if response.status_code == 200:
             return int(response.text.split("<u8>")[1].split("</u8>")[0])
         return None
 
     def set_play_state(self, state):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/SET/netRemote.play.control?pin={self.pin}&sid={self.session_id}&value={state}")
-        return response.status_code == 200
+        return self.request("SET", "netRemote.play.control", {"value": state}).status_code == 200
 
     def get_play_info_duration(self):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/GET/netRemote.play.info.duration?pin={self.pin}&sid={self.session_id}")
+        response = self.request("GET", "netRemote.play.info.duration")
         if response.status_code == 200:
             return int(response.text.split("<u32>")[1].split("</u32>")[0])
         return None
 
     def get_mode(self):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/GET/netRemote.sys.mode?pin={self.pin}&sid={self.session_id}")
+        response = self.request("GET", "netRemote.sys.mode")
         if response.status_code == 200:
             return int(response.text.split("<u32>")[1].split("</u32>")[0])
         return None
     
     def set_mode(self, mode):
-        if not self.session_id:
-            self._get_session_id()
-        response = requests.get(f"{self.base_url}/SET/netRemote.sys.mode?pin={self.pin}&sid={self.session_id}&value={mode}")
-        return response.status_code == 200
-
-    def send_keep_alive_request(self):
+        return self.request("SET", "netRemote.sys.mode", {"value": mode}).status_code == 200
+        
+    def send_keep_aliverequest(self):
         if self.get_mode() != 1 and self.get_play_status() == 0 and self.get_play_info_duration() != 0:
             # Speaker is in non-Bluetooth/Aux Mode, but playing music through cable
             # If we try to set play state to 2 (pause) - it will cause music to start playing
@@ -149,7 +151,7 @@ def keep_jbl_up(args):
         try:
             if args.pc_address is None or is_pc_up(args.pc_address):
                 # Send keep alive request to the speaker
-                if jbl.send_keep_alive_request():
+                if jbl.send_keep_aliverequest():
                     logger.info("Keep Alive Request Sent")
                 else:
                     logger.error("Failed to send keep alive request to JBL speaker")
